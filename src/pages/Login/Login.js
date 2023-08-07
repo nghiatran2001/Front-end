9997890b6642e4b0b315b5d582e17f0af87a9358
nghiatran2001 <trannguyenhuunghia6@gmail.com>
2023-08-07 15:22:50 +0700

    update

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,51 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button, Input, InputLabel } from "@mui/material";
 import "./Login.css";
-import { notification } from "antd";
-import { Form, Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { user as userAPI } from "../../API/index";
-import userSlice from "../../redux/userSlice";
+
+import { Form, Link } from "react-router-dom";
 
 export default function Login() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const [api, contextHolder] = notification.useNotification();
-
-  const [loginInfo, setLoginInfo] = useState({});
-
-  const login = async (e) => {
-    e.preventDefault();
-    try {
-      const result = await userAPI.login(loginInfo);
-      if (result.data.user.disable) {
-        api.open({
-          type: "error",
-          message: "Tài khỏan bạn đã bị khóa vui lòng liên hệ admin để mở!!!!",
-        });
-      } else {
-        dispatch(userSlice.actions.setUser(result.data.user));
-        document.cookie = `token=${result.data.refreshToken}`;
-        navigate("/");
-      }
-    } catch (error) {
-      api.open({
-        type: "error",
-        message: "Email hoặc mật khẩu không chính xác.",
-      });
-    }
-  };
-
-  const handleLoginInfoChange = (key, value) => {
-    setLoginInfo((pre) => {
-      return { ...pre, [key]: value };
-    });
-  };
-
   return (
     <div>
-      {contextHolder}
       <div className="auth">
         <h1>ĐĂNG NHẬP</h1>
         <Form className="form">
@@ -55,9 +16,6 @@ export default function Login() {
             type="email"
             placeholder="Email"
             className="input"
-            onChange={(e) => {
-              handleLoginInfoChange("email", e.target.value);
-            }}
           ></Input>
           <InputLabel className="label">Mật Khẩu</InputLabel>
           <Input
@@ -65,14 +23,8 @@ export default function Login() {
             type="password"
             placeholder="Mật khẩu"
             className="input"
-            onChange={(e) => {
-              e.preventDefault();
-              handleLoginInfoChange("password", e.target.value);
-            }}
           ></Input>
-          <Button className="button" onClick={login}>
-            Đăng nhập
-          </Button>
+          <Button className="button">Đăng nhập</Button>
           <span>
             Bạn chưa có tài khoản? <Link to="/register">Đăng ký</Link>
           </span>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,99 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./Register.css";
 import { Button, Input, InputLabel } from "@mui/material";
-import { notification } from "antd";
+
 import { Form, Link } from "react-router-dom";
 
-import { user as userAPI } from "../../API/index";
-import { isEmpty, isEmail } from "validator";
-
 export default function Register() {
-  const [api, contextHolder] = notification.useNotification();
-
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [retypePassword, setRetypePassword] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const [validation, setValidation] = useState("");
-
-  const handleFullNameChange = (e) => {
-    setFullName(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleRetypePasswordChange = (e) => {
-    setRetypePassword(e.target.value);
-  };
-  const handlePhoneChange = (e) => {
-    setPhone(e.target.value);
-  };
-
-  const validateAll = () => {
-    const msg = {};
-    if (isEmpty(fullName)) {
-      msg.fullName = "Vui lòng nhập tên";
-    }
-    if (isEmpty(email)) {
-      msg.email = "Vui lòng nhập email";
-    } else if (!isEmail(email)) {
-      msg.email = "Email không hợp lệ";
-    }
-    if (isEmpty(password)) {
-      msg.password = "Vui lòng nhập mật khẩu";
-    }
-    if (isEmpty(retypePassword)) {
-      msg.retypePassword = "Vui lòng nhập lại mật khẩu";
-    }
-    if (isEmpty(phone)) {
-      msg.phone = "Vui lòng nhập số điện thoại";
-    }
-    setValidation(msg);
-    if (Object.keys(msg).length > 0) return false;
-    return true;
-  };
-
-  const register = async (e) => {
-    e.preventDefault();
-
-    const isValid = validateAll();
-    if (!isValid) return;
-    else {
-      try {
-        const result = await userAPI.register({
-          name: fullName,
-          email,
-          password,
-          phone,
-        });
-        if (result.status === 200) {
-          api.open({
-            type: "success",
-            message: "Đăng ký thành công.",
-          });
-          setFullName("");
-          setEmail("");
-          setPassword("");
-          setRetypePassword("");
-          setPhone("");
-        }
-      } catch (error) {
-        api.open({
-          type: "error",
-          message: "Email đã tồn tại.",
-        });
-      }
-    }
-  };
   return (
     <div>
-      {contextHolder}
       <div className="auth">
         <h1>ĐĂNG KÝ</h1>
         <Form className="form">
@@ -104,10 +18,8 @@ export default function Register() {
             id="username"
             name="username"
             className="input"
-            value={fullName}
-            onChange={handleFullNameChange}
           ></Input>
-          <p>{validation.fullName}</p>
+
           <InputLabel className="label">Email</InputLabel>
           <Input
             required
@@ -115,10 +27,8 @@ export default function Register() {
             id="email"
             name="email"
             className="input"
-            value={email}
-            onChange={handleEmailChange}
           ></Input>
-          <p>{validation.email}</p>
+
           <InputLabel className="label">Mật Khẩu</InputLabel>
           <Input
             required
@@ -126,20 +36,17 @@ export default function Register() {
             id="password"
             name="password"
             className="input"
-            value={password}
-            onChange={handlePasswordChange}
           ></Input>
-          <InputLabel className="label">Mật Khẩu</InputLabel>
+
+          <InputLabel className="label">Nhập lại mật Khẩu</InputLabel>
           <Input
             required
             type="password"
             className="input"
             id="confirmPassword"
             name="confirmPassword"
-            value={retypePassword}
-            onChange={handleRetypePasswordChange}
           ></Input>
-          <p>{validation.retypePassword}</p>
+
           <InputLabel className="label">Số Điện Thoại</InputLabel>
           <Input
             required
@@ -147,13 +54,9 @@ export default function Register() {
             id="phone"
             name="phone"
             className="input"
-            value={phone}
-            onChange={handlePhoneChange}
           ></Input>
-          <p>{validation.phone}</p>
-          <Button className="button" onClick={register}>
-            Đăng ký
-          </Button>
+
+          <Button className="button">Đăng ký</Button>
           <span>
             Bạn đã có tài khoản? <Link to="/login">Đăng nhập</Link>
           </span>
